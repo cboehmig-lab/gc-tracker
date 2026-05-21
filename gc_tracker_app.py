6630092b7140e9029d2f32ee69492ceb858f1015
Chuck Boehmig <cboehmig@gmail.com>
2026-05-21 11:52:52 -0500

    v2.12.15: fix sticky table headers (border-collapse:separate)

--- gc_tracker_app.py
+++ gc_tracker_app.py
@@ -5221,7 +5221,7 @@ if GA_MEASUREMENT_ID:
     )
 else:
     _ga_snippet = ''
-APP_VERSION = "2.12.14"
+APP_VERSION = "2.12.15"
 HTML_TEMPLATE = HTML_TEMPLATE.replace('<!-- __GA__ -->', _ga_snippet)
 HTML_TEMPLATE = HTML_TEMPLATE.replace('<!-- __VER__ -->', f'v{APP_VERSION}')
 CL_TEMPLATE   = CL_TEMPLATE.replace('<!-- __GA__ -->', _ga_snippet)
